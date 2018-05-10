8f2d842a8eced4f23564e60ef312661cc47191d8
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2018-05-10 08:13:15 -0400

    Updated from Brython Server: 5/10/2018 8:13:15 AM

--- longestDictionaryWord.py
+++ longestDictionaryWord.py
@@ -1,1 +1,10 @@
+#Matthew Siegel
+#5/10/18
+#longestDictionaryWord.py - finds longest word
 
+file = open('engmix.txt')
+count = 0
+for line in file:
+    if len(line) > count:
+        count = len(line)
+print(count)
