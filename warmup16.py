2740b2b4ccafcfd040851ec9144e52cfd324abbf
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2018-05-11 08:20:20 -0400

    Updated from Brython Server: 5/11/2018 8:20:21 AM

--- warmup16.py
+++ warmup16.py
@@ -1,1 +1,12 @@
+#Matthew Siegel
+#5/9/18
+#fileDemo.py - how to read a file
 
+file = open('engmix.txt')
+
+
+for line in file:
+    if line[0] == 'm' and line[-1] == 's'
+        print(line.strip())
+    
+    
