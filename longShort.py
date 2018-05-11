a06ba7f42d157e51edc6d4cd3cea8c6b47cac902
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2018-05-11 08:40:16 -0400

    Updated from Brython Server: 5/11/2018 8:40:17 AM

--- longShort.py
+++ longShort.py
@@ -1,1 +1,18 @@
+#Matthew Siegel
+#5/11/18
+#longShort.py - finding longest and shortest word of each letter
 
+file = open('engmix.txt')
+
+data = {}
+for ch in 'abcdefghijklmnopqrstuvwxyz'
+    data[ch:'shortest'] = ''
+    data[ch:'longest'] = ''
+
+for line in file:
+    line = line.strip()
+    firstLetter = line[0]
+    if len(line) < len(data[firstLetter:'shortest']):
+        data[ch:'shortest'] = line
+    if len(line) > len(data[firstLetter:'longest']):
+        data[ch:'longest'] = line
