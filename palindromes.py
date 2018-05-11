4dc24ea508fb1c6c5567edcf2a941509f8979ede
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2018-05-11 08:29:44 -0400

    Updated from Brython Server: 5/11/2018 8:29:45 AM

--- palindromes.py
+++ palindromes.py
@@ -5,8 +5,9 @@
 file = open('engmix.txt')
 
 for line in file:
+    line = line.strip()
     word = ""
-    for i in range(len(line),0,-1):
+    for i in range(len(line)-1,0,-1):
         word += line[i]
-    if line ==- word:
+    if line == word:
         print(word)
