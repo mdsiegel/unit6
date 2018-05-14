d23f18ee53f18ec33d106b2494f4cb15d90c7a3f
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2018-05-14 08:05:47 -0400

    Updated from Brython Server: 5/14/2018 8:05:47 AM

--- HW6.py
+++ HW6.py
@@ -1,1 +1,14 @@
+#Matthew Siegel
+#5/14/18
+#HW6.py 
+
+file = open('engmix.txt')
+
+#1
+word = input('Enter a word: ')
+for line in file:
+    if word in line.strip():
+        print(line.strip())
+
+    
 
