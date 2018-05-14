993126e57840375691ed05cc1a73035aa9fc3e1c
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2018-05-14 08:48:24 -0400

    Updated from Brython Server: 5/14/2018 8:48:24 AM

--- longShort.py
+++ longShort.py
@@ -6,24 +6,30 @@ file = open('engmix.txt')
 
 lShort = ['']*26
 lLong = ['']*26
-lAlph = ['']*26
 
 
-alph = 'abcdefghijklmnopqrstuvwxyz'
+alph = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
     
     
 
 for line in file:
     line = line.strip()
-    if len(line) > 0:
+    if len(line) > 0 and line[0] != ' ':
         firstLetter = line[0]
-        print(line)
         if line[0] not in alph:
             print(line[0])
-        numLetter = alph.index(firstLetter)
+            print(False)
+            print(line)
+            numLetter = 2
+        else:
+            numLetter = alph.index(firstLetter)
         shortestWord = lShort[numLetter]
         longestWord = lLong[numLetter]
         if len(line) < len(shortestWord):
             lShort[numLetter] = line
         if len(line) > len(longestWord):
             lLong[numLetter] = line
+
+
+print(lShort)
+print(lLong)
