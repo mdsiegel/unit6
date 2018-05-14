044718de754de36b0b185b5e949783e102404c1b
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2018-05-14 08:10:57 -0400

    Updated from Brython Server: 5/14/2018 8:10:58 AM

--- HW6.py
+++ HW6.py
@@ -5,10 +5,22 @@
 file = open('engmix.txt')
 
 #1
+"""
 word = input('Enter a word: ')
 for line in file:
     if word == line.strip():
         print('It is in the dictionary')
+        """
+
+#2
+L = []
+for line in file:
+    L.append(line.strip())
+num = int(input('Enter a number: '))
+if num >= 0 and num <= len(L):
+    print(L[num])
+else:
+    print('not in range')
 
     
 
