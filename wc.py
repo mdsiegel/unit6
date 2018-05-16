3b92bf537316621c83b108839358b54cce73052a
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2018-05-16 08:34:32 -0400

    Updated from Brython Server: 5/16/2018 8:34:31 AM

--- wc.py
+++ wc.py
@@ -1,1 +1,13 @@
+#Matthew Siegel
+#5/16/18
+#wc.py - finding stuff about files
 
+fileName = input('Enter a file name: ')
+file = open(fileName)
+
+def WordCount():
+    count = 0
+    file = file.strip()
+    for line in file:
+        count += len(line)
+    print
