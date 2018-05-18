bd812c13d9ee913a6a5b01c73be3e4ecaf01f182
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2018-05-18 08:48:39 -0400

    Updated from Brython Server: 5/18/2018 8:48:39 AM

--- anagram.py
+++ anagram.py
@@ -11,11 +11,5 @@ for ch in word:
 length = len(letters)
 for line in file:
     line = line.strip()
-    for i in range(0,length+1):
-        good = True
-        if letters[i] not in line:
-            good  = False
-        if i == length + 1:
-            if good == True:
-                print(line)
+    
         
