8e9aa7615fa0825d389d71c813d474586ba0f330
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2018-05-18 08:39:07 -0400

    Updated from Brython Server: 5/18/2018 8:39:06 AM

--- anagram.py
+++ anagram.py
@@ -1,1 +1,14 @@
+#Matthew Siegel
+#5/18/18
+#anagram.py 
 
+file = open('engmix.txt')
+word = input('Enter a word: ')
+letters = []
+for ch in word:
+    letters.append(ch)
+
+for line in file:
+    for l in letters:
+        if l in line:
+            print('line')
