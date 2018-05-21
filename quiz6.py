bae930a45b19295bf0478438c98f1c6a664cc2bd
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2018-05-21 08:10:28 -0400

    Updated from Brython Server: 5/21/2018 8:10:30 AM

--- quiz6.py
+++ quiz6.py
@@ -5,7 +5,17 @@
 file = open('engmix.txt')
 
 #1
+'''
 letter = (input('Enter a letter: ')).lower()
 for line in file:
     if line.count(letter) == 4:
         print(line)
+        
+'''
+
+#2
+for line in file:
+    if len(line) >= 9:
+        if line[0] == line[4] and line[0] == line[8]:
+            print(line)
+            break
