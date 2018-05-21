2e081010c3b6034ff011f24eafcb2039a0249ae2
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2018-05-21 08:07:12 -0400

    Updated from Brython Server: 5/21/2018 8:07:14 AM

--- quiz6.py
+++ quiz6.py
@@ -1,1 +1,11 @@
+#Matthew Siegel
+#5/21/18
+#quiz6.py
 
+file = open('engmix.txt')
+
+#1
+letter = (input('Enter a letter: ')).lower()
+for line in file:
+    if line.count(letter) == 4:
+        print(line)
