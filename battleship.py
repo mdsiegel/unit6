2bdf4ad5509edcaae49a70a9ab76d7c364615f19
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2018-05-23 08:37:58 -0400

    Updated from Brython Server: 5/23/2018 8:38:01 AM

--- battleship.py
+++ battleship.py
@@ -2,4 +2,13 @@
 #5/23/18
 #battleship.py
 
+from ggame import *
 
+red = Color(0xFF0000,1)
+green = Color(0x00FF00,1)
+blue = Color(0x0000FF,1)
+black = Color(0x000000,1)
+
+
+def buildBoard():
+    board = 
