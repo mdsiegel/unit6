9b9f69019efc8ea98d765a0022d927b9aebd1434
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2018-05-24 08:18:48 -0400

    Updated from Brython Server: 5/24/2018 8:18:49 AM

--- battleship.py
+++ battleship.py
@@ -8,7 +8,18 @@ red = Color(0xFF0000,1)
 green = Color(0x00FF00,1)
 blue = Color(0x0000FF,1)
 black = Color(0x000000,1)
+white = Color(0xFFFFFF,1)
 
 
 def buildBoard():
-    board = 
+    blackOutline = LineStyle(1,black) #pixels,color
+    board1 = [[0,0,0],[0,0,0],[0,0,0]]
+    for r in range(0,4):
+        for c in range(0,4):
+            box = RectangleAsset(100,100,blackOutline,white)
+            Sprite(box, (0+(100*r),0+(100*c)))
+
+
+if __name__ == '__main__':
+    buildBoard()
+    App().run()
