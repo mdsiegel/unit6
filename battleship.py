e0092e869c263d260e5b7a8cfde2f7070e3ba77c
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2018-05-24 08:39:50 -0400

    Updated from Brython Server: 5/24/2018 8:39:47 AM

--- battleship.py
+++ battleship.py
@@ -10,13 +10,13 @@ green = Color(0x00FF00,1)
 blue = Color(0x0000FF,1)
 black = Color(0x000000,1)
 white = Color(0xFFFFFF,1)
-
+PICKSHIPS = True
 
 def buildBoard():
     blackOutline = LineStyle(1,black) #pixels,color
     for r in range(0,4):
         for c in range(0,4):
-            if board1[r][c] == 2:
+            if board1[r][c] == 1:
                 box = RectangleAsset(100,100,blackOutline,red)
             else:
                 box = RectangleAsset(100,100,blackOutline,white)
@@ -26,6 +26,14 @@ def buildBoard():
             box = RectangleAsset(100,100,blackOutline,white)
             Sprite(box, (500+(100*r),0+(100*c)))
 
+def findBox(clickx,clicky):
+    boxLength = 100
+    boxHeight = 100
+    for r in range(0,4):
+        for c in range(0,4):
+            if clickx > 100*i and clickx < 100*(i+1) and clicky > 100*i and clicky < 100*(i+1):
+    return (r,c) 
+
 def pickShips():
     for i in range(0,3):
         r1 = randint(0,3)
@@ -33,6 +41,11 @@ def pickShips():
         board1[r1][r2] = 2
     print('Pick Ships')
     
+def mouseClick(event):
+    x = event.x
+    y = event.y
+    if PICKSHIPS == True:
+        
 
 
 if __name__ == '__main__':
@@ -40,4 +53,5 @@ if __name__ == '__main__':
     board2 = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
     pickShips()
     buildBoard()
+    App().listenMouseEvent('click', mouseClick)
     App().run()
