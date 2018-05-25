695353e8fc4100e46e887a468f02cd413b970e62
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2018-05-25 08:21:15 -0400

    Updated from Brython Server: 5/25/2018 8:21:15 AM

--- battleship.py
+++ battleship.py
@@ -58,6 +58,7 @@ def mouseClick(event):
     else:
         (r,c) = findBox(x,y)
         if board1[r][c] == 2:
+            board1[r][c] == 1
             print('got it')
             board1[r][c] == 1
             buildBoard()
